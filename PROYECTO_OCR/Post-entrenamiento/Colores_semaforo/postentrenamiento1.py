853166faf91ae8f28421c86e11c17bac69c5c2f9
Andres <jaromero@unitru.edu.pe>
2024-02-11 09:03:21 -0500

    Actualizado

--- PROYECTO_OCR/Post-entrenamiento/Colores_semaforo/postentrenamiento1.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt")
-
-model.train(data = "Post-entrenamiento\Colores_semaforo\config1.yaml" , epochs=15, imgsz=640)
-
--- PROYECTO_OCR/Post-entrenamiento/Franja_peatonal/postentrenamiento2.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt")
-
-model.train(data = "Post-entrenamiento\Franja_peatonal\config2.yaml" , epochs=20, imgsz=640)
-
