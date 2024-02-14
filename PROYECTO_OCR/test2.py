093b2f1ff42b6c3d567c438585fe0e5ecc4d8cf4
Andres <jaromero@unitru.edu.pe>
2024-02-14 13:11:02 -0500

    arreglado

--- PROYECTO_OCR/test2.py
+++ PROYECTO_OCR/test2.py
@@ -24,24 +24,23 @@ if __name__ =='__main__':
         
         results = model(frame, stream=True)
 
-        for res in results:
-            indice_filtro = np.where((np.isin(res.boxes.cls.cpu().numpy(), [0,1,2,4,6,7,8])) & (res.boxes.conf.cpu().numpy() > 0.5)) [0]
-            boxes = res.boxes.xyxy.cpu().numpy() [indice_filtro].astype(int)
+        for res1 in results:
+            indice_filtro1 = np.where((np.isin(res1.boxes.cls.cpu().numpy(), [0,1,2,4,6,7,8])) & (res1.boxes.conf.cpu().numpy() > 0.5)) [0]
+            boxes1 = res1.boxes.xyxy.cpu().numpy() [indice_filtro1].astype(int)
 
-            tracks = tracker.update(boxes)
-            tracks = tracks.astype(int)
+            tracks1 = tracker.update(boxes1)
+            tracks1 = tracks1.astype(int)
 
-            for xmin, ymin, xmax, ymax, track_id in tracks:
+            for xmin, ymin, xmax, ymax, track_id in tracks1:
 
                 xc, yc = int((xmin+xmax)/2), ymin
-
+                
                 if track_id not in cruza_linea_roja:
                     cruza_rojo = (RED_LINE[1][0] - RED_LINE[0][0]) * (yc - RED_LINE[0][1]) - (RED_LINE[1][1] - RED_LINE[0][1]) * (xc - RED_LINE[0][0])
                     if cruza_rojo >= 0:
-                        cruza_linea_roja[track_id]={
-                            print("Objeto delante"),
-                            print("  ")
-                        }
+                        cruza_linea_roja[track_id]=True
+                        print(f"objeto {track_id} cruzó la línea roja")
+                        
 
                 cv2.circle(img=frame, center=(xc,yc), radius=5, color=(0, 255, 0), thickness=-1)
                 cv2.rectangle(img=frame, pt1=(xmin,ymin), pt2=(xmax, ymax), color=(255, 255, 0), thickness=2)
